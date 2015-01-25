92108002857f964b9f403f1e8e97120fc214caf0
Martin Andreev <martin.andreev7@gmail.com>
2015-01-25 18:41:27 +0200

    Exception-Handling

---
+++ Exception-Handling/01. Square Root/SquareRoot.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _01.Square_Root
+{
+    class SquareRoot
+    {
+        static void Main()
+        {
+            Console.Write("Input a number: ");
+
+            try
+            {
+                int num = int.Parse(Console.ReadLine());
+
+                if (num < 0)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+
+                Console.WriteLine("The square root of {0} is {1}", num, Math.Sqrt(num));
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid number!");
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("Invalid number!");
+            }
+            finally
+            {
+                Console.WriteLine("Good bye!");
+            }
+        }
+    }
+}
---
+++ Exception-Handling/02. Enter Numbers/EnterNumbersInRange.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace _02.Enter_Numbers
+{
+    class EnterNumbersInRange
+    {
+        static void Main()
+        {
+            int start = 1;
+            int end = 99;
+            int counter = 9;
+
+            for (int i = 0; i < 10; i++)
+            {
+                end = 99 - counter;
+                
+                bool isValid = false;
+                
+                do
+                {
+                    Console.Write("Number {0}: ", i + 1);
+                    try
+                    {
+                        start = ReadNumber(start, end);
+                        counter--;
+                        isValid = true;
+                    }
+                    catch (ArgumentOutOfRangeException)
+                    {
+                        Console.WriteLine("Try again! Your number must be in range {0} - {1}", start + 1, end);
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("Invalid number!");
+                    }
+                } while (!isValid);
+            }
+        }
+
+        private static int ReadNumber(int start, int end)
+        {
+            int number = 0;
+            number = int.Parse(Console.ReadLine());
+            if (number <= start || number > end)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            return number;
+        }
+    }
+}
